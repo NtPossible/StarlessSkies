cd7ddba3b523a82ea0768d94cd7c14a73c886b30
NtPossible <64660446+NtPossible@users.noreply.github.com.>
2025-08-15 19:31:13 +0200

    Add project files.

---
+++ StarlessSkies/StarlessSkiesModSystem.cs
@@ -0,0 +1,17 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace StarlessSkies
+{
+    public class StarlessSkiesModSystem : ModSystem
+    {
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.WorldManager.SetSunBrightness(0);
+            float[] zeroSunLevels = new float[32];
+            api.WorldManager.SetSunLightLevels(zeroSunLevels);
+        }
+    }
+}
